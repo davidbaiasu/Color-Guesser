4a47de48c9f563b4e0307e5880573b1f4d0a02ff
Baiasu <baiasudavidalin@gmail.com>
2026-03-03 21:26:31 +0200

    Added button and slider functionality

--- colorguesser.js
+++ colorguesser.js
@@ -1,6 +1,41 @@
 console.log("Hello!");
 
+const colorSquareElement = document.getElementById('id-div-color-square');
+const sliderElements = document.querySelectorAll('.class-range');
+const redSlider = document.getElementById('id-range-red');
+const greenSlider = document.getElementById('id-range-green');
+const blueSlider = document.getElementById('id-range-blue');
+const submitElement = document.getElementById('id-button-submit');
+const attemptsSpan = document.getElementById('id-span-attempt');
+
 const MAX_COLOR_CODE = 255;
+const MAX_ATTEMPTS = 10;
+
+sliderElements.forEach(sliderElements => {
+    sliderElements.addEventListener('input', (event) => {
+        const currentSlider = event.target;
+        const newValue = currentSlider.value;
+        const valueSpan = currentSlider.nextElementSibling;
+        valueSpan.textContent = newValue;
+    });
+});
+
+submitElement.addEventListener('click', () => {
+	const redValue   = Number(redSlider.value);
+	const greenValue = Number(greenSlider.value);
+	const blueValue  = Number(blueSlider.value);
+	inputColor.updateColor(redValue, greenValue, blueValue);
+	
+	let attemptNumber = Number(attemptsSpan.innerHTML);
+	if( attemptNumber < MAX_ATTEMPTS ){
+		attemptNumber++;
+		attemptsSpan.innerHTML = `${attemptNumber}` ;
+	}
+	if( attemptNumber == MAX_ATTEMPTS ){
+		////
+	}
+	
+});
 
 class Color{
 	
@@ -22,6 +57,12 @@ class Color{
 		return Math.floor(Math.random() * (MAX_COLOR_CODE + 1));
 	}
 	
+	updateColor(r, g, b){
+		this.valueRed = r;
+		this.valueGreen = g;
+		this.valueBlue = b;
+	}
+	
 	rgbToString(){
 		return `rgb(${this.valueRed}, ${this.valueGreen}, ${this.valueBlue})`;
 	}
@@ -32,6 +73,9 @@ class Color{
 	
 }
 
-let colorObj = new Color();
+const targetColor = new Color();
+const inputColor = new Color();
 
-colorObj.tableColors();
+colorSquareElement.style.backgroundColor = targetColor.rgbToString();
+targetColor.tableColors();
+inputColor.tableColors();
